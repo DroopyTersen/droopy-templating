6121150e3d68aa75cdbd50f5d940de6de38b6a7e
unknown <apetersen@LT0927.skyline.gb>
2015-03-20 10:29:45 -0500

    initial commit

---
+++ droopy-templating.js
@@ -0,0 +1,97 @@
+var templating = {
+
+	Placeholder: function(raw) {
+		this.raw = raw;
+		this.fullProperty = raw.slice(2, raw.length - 2);
+	},
+
+	getPlaceHolders: function(template, regexp) {
+		var regExpPattern = regexp || /\{\{[^\}]+\}\}?/g;
+		var matches = template.match(regExpPattern);
+		return matches || [];
+	},
+
+	getObjectValue: function(obj, fullProperty) {
+		var value = obj,
+			propertyChain = fullProperty.split('.');
+
+		for (var i = 0; i < propertyChain.length; i++) {
+			var property = propertyChain[i];
+			value = value[property] != null ? value[property] : "Not Found: " + fullProperty;
+		}
+
+		if(fullProperty === "_") {
+			value = obj;
+		}
+		
+		if ((typeof value === "string") && value.indexOf("/Date(") !== -1) {
+			var dateValue = UTCJsonToDate(value);
+			value = dateValue.toLocaleDateString();
+		}
+
+		return value;
+	},
+
+	populateTemplate: function(template, item, regexp) {
+		var placeholders = this.getPlaceHolders(template, regexp) || [],
+			itemHtml = template;
+
+		for (var i = 0; i < placeholders.length; i++) {
+			var placeholder = new this.Placeholder(placeholders[i]);
+			placeholder.val = this.getObjectValue(item, placeholder.fullProperty);
+			var pattern = placeholder.raw.replace("[", "\\[").replace("]", "\\]");
+			var modifier = "g";
+			itemHtml = itemHtml.replace(new RegExp(pattern, modifier), placeholder.val);
+		}
+		return itemHtml;
+	}
+};
+
+templating.Each = {
+
+	regExp: /\{\[[^\]]+\]\}?/g,
+
+	populateEachTemplates: function(itemHtml, item) {
+		var $itemHtml = $(itemHtml),
+			eachTemplates = $itemHtml.find("[data-each]");
+
+		eachTemplates.each(function() {
+			var arrayHtml = "",
+				itemTemplate = $(this).html(),
+				arrayProp = $(this).data("each"),
+				array = sp.templating.getObjectValue(item, arrayProp);
+
+			if (array != null && $.isArray(array)) {
+				for (var i = 0; i < array.length; i++) {
+					arrayHtml += templating.populateTemplate(itemTemplate, array[i], templating.Each.regExp);
+				}
+			}
+
+			$itemHtml.find($(this)).html(arrayHtml);
+		});
+
+		var temp = $itemHtml.clone().wrap("<div>");
+		return temp.parent().html();
+	}
+};
+
+templating.renderTemplate = function(template, item, renderEachTemplate) {
+	var itemHtml = templating.populateTemplate(template, item);
+	if (renderEachTemplate) {
+		itemHtml = templating.Each.populateEachTemplates(itemHtml, item);
+	}
+	return itemHtml;
+};
+
+var UTCJsonToDate = function(jsonDate) {
+	var utcStr = jsonDate.substring(jsonDate.indexOf("(") + 1);
+	utcStr = utcStr.substring(0, utcStr.indexOf(")"));
+
+	var returnDate = new Date(parseInt(utcStr, 10));
+	var hourOffset = returnDate.getTimezoneOffset() / 60;
+	returnDate.setHours(returnDate.getHours() + hourOffset);
+
+	return returnDate;
+};
+
+module.exports = templating;
